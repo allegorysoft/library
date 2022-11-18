dadd794994b06133add6f4372a9eb61337e96de9
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-11-18 18:24:18 +0300

    Powershell script changed

--- abp/templates/delete-builds.ps1
+++ abp/templates/delete-builds.ps1
@@ -1,3 +1,3 @@
-Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
+Get-ChildItem .\ -include bin,obj,.idea -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 
 Get-ChildItem -h .\ -include .vs -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
